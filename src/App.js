fe2dbe626bf07fd661444c75104d81677c583c8b
marcin-bojar <marcin.bojar@interia.pl>
2020-07-04 12:52:21 +0200

    Initial commit - basic setup + CardList component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await res.json();
+    this.setState({ monsters: users });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <CardList>
+          { 
+            this.state.monsters.map(el => (
+            <h1 key={el.id}> {el.name} </h1>
+          ))}
+        </CardList>    
+      </div>  
+    );
+  }
 }
 
 export default App;
