2d1237bbf9097b2102ed95fdfb896dd5030ed050
marcin-bojar <marcin.bojar@interia.pl>
2020-07-07 00:07:19 +0200

    Updates

---
+++ precache-manifest.8a490a61f7b86b289b65528e8ca3698c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e4d82d42fa3c63c18391016d67377e",
+    "url": "/marcin-bojar/monsters/index.html"
+  },
+  {
+    "revision": "feb46b6d019f88489aa0",
+    "url": "/marcin-bojar/monsters/static/css/main.ba501845.chunk.css"
+  },
+  {
+    "revision": "de26e83c8d55bd12aa90",
+    "url": "/marcin-bojar/monsters/static/js/2.ab8b341c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcin-bojar/monsters/static/js/2.ab8b341c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb46b6d019f88489aa0",
+    "url": "/marcin-bojar/monsters/static/js/main.a52727a7.chunk.js"
+  },
+  {
+    "revision": "bb15643403aae00ce374",
+    "url": "/marcin-bojar/monsters/static/js/runtime-main.4f5442b5.js"
+  }
+]);
